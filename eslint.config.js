22906c4ea6a043359b61e6c7fbf3687cb3176082
ryufuta <rfutago@gmail.com>
2026-06-25 14:33:32 +0900

    prettierを実行

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,10 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import prettier from 'eslint-config-prettier'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import js from '@eslint/js';
+import globals from 'globals';
+import prettier from 'eslint-config-prettier';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
+import { defineConfig, globalIgnores } from 'eslint/config';
 
 export default defineConfig([
   globalIgnores(['dist']),
@@ -21,4 +21,4 @@ export default defineConfig([
     },
   },
   prettier,
-])
+]);
