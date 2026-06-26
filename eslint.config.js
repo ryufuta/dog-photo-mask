c9b2788d325af47c61e9c9e8d9fab113b4dc9ebf
ryufuta <rfutago@gmail.com>
2026-06-26 12:57:59 +0900

    importの並び順を整理
    `eslint.config.js`もlint対象に含める

--- eslint.config.js
+++ eslint.config.js
@@ -1,18 +1,18 @@
-import eslintReact from '@eslint-react/eslint-plugin';
 import js from '@eslint/js';
-import globals from 'globals';
+import eslintReact from '@eslint-react/eslint-plugin';
+import { defineConfig, globalIgnores } from 'eslint/config';
 import prettier from 'eslint-config-prettier';
+import jsxA11y from 'eslint-plugin-jsx-a11y';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
-import jsxA11y from 'eslint-plugin-jsx-a11y';
-import tseslint from 'typescript-eslint';
 import simpleImportSort from 'eslint-plugin-simple-import-sort';
-import { defineConfig, globalIgnores } from 'eslint/config';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
 
 export default defineConfig([
   globalIgnores(['dist']),
   {
-    files: ['**/*.{ts,tsx}'],
+    files: ['**/*.{js,ts,tsx}'],
     extends: [
       js.configs.recommended,
       tseslint.configs.recommended,
