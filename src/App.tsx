b31c178bb3dce64f44b83d51fbc2572660c13790
ryufuta <rfutago@gmail.com>
2026-07-12 14:59:53 +0900

    `src/index.css`をTailwind CSSで書き換え
    - 未使用のプロパティを削除
    - 見た目上重要でないスタイルは後で調整するため削除
    - Tailwind CSSのデフォルト値と同じものを削除
    - h1にユーティリティクラスを使用

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,9 @@ import { ImageEditor } from '@/ImageEditor.tsx';
 function App() {
   return (
     <>
-      <h1>Dog Photo Mask</h1>
+      <h1 className="my-5 text-center text-4xl font-medium tracking-tight text-[var(--text-h)] lg:my-8 lg:text-6xl">
+        Dog Photo Mask
+      </h1>
       <ImageEditor />
     </>
   );
