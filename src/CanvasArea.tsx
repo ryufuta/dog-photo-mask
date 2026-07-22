a1758eadf9e96c403c34cc54e3321c1d923ca417
ryufuta <rfutago@gmail.com>
2026-07-22 17:24:49 +0900

    HTML要素のサイズを動的に取得するカスタムフックを実装

--- src/CanvasArea.tsx
+++ src/CanvasArea.tsx
@@ -1,31 +1,12 @@
-import { useLayoutEffect, useRef, useState } from 'react';
 import { Image, Layer, Stage } from 'react-konva';
+import { useElementSize } from '@/useElementSize.ts';
 
 export function CanvasArea({ image }: { image: HTMLImageElement }) {
-  const [stageSize, setStageSize] = useState({ width: 0, height: 0 });
-  const containerRef = useRef<HTMLDivElement>(null);
-
-  useLayoutEffect(() => {
-    const targetElement = containerRef.current;
-    if (!targetElement) return;
-
-    const observer = new ResizeObserver(([entry]) => {
-      if (!entry) return;
-
-      const { inlineSize, blockSize } = entry.contentBoxSize[0];
-      setStageSize({
-        width: inlineSize,
-        height: blockSize,
-      });
-    });
-
-    observer.observe(targetElement);
-    return () => observer.disconnect();
-  }, []);
+  const [setRef, size] = useElementSize<HTMLDivElement>();
 
   return (
-    <div ref={containerRef} className="bg-surface flex-1 overflow-hidden">
-      <Stage width={stageSize.width} height={stageSize.height}>
+    <div ref={setRef} className="bg-surface flex-1 overflow-hidden">
+      <Stage width={size.width} height={size.height}>
         <Layer>
           <Image image={image} scaleX={0.5} scaleY={0.5} />
         </Layer>
