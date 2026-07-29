d557b7a919dfdfcf3e3f5c8da9bec0720989f2b1
ryufuta <rfutago@gmail.com>
2026-07-29 09:19:25 +0900

    Vitest Browser Modeをインストール

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 /// <reference types="vitest/config" />
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
+import { playwright } from '@vitest/browser-playwright';
 import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
@@ -11,5 +12,10 @@ export default defineConfig({
   },
   test: {
     globals: true,
+    browser: {
+      enabled: true,
+      provider: playwright(),
+      instances: [{ browser: 'chromium' }],
+    },
   },
 });
