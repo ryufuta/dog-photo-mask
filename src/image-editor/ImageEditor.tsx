03b0d7923d74c7f53a241992b56482c1db9dbcf4
ryufuta <rfutago@gmail.com>
2026-09-12 10:54:08 +0900

    画像をアップロードすると画像読み込み中画面に遷移する
    - UX改善
      - 前の画像の読み込み中に別の画像をアップロードできる問題を解決
      - アプリの状態を表示することで読み込みに時間がかかる場合のユーザーのストレスを軽減
    - 設計改善
      - UploadScreenの責務をファイルのアップロードのみにする

--- src/image-editor/ImageEditor.tsx
+++ src/image-editor/ImageEditor.tsx
@@ -2,17 +2,51 @@ import { useState } from 'react';
 import { EditorScreen } from './editor-screen/EditorScreen.tsx';
 import { UploadScreen } from './upload-screen/UploadScreen.tsx';
 
-export function ImageEditor() {
-  const [image, setImage] = useState<HTMLImageElement | null>(null);
+type State =
+  | { type: 'upload' }
+  | { type: 'loading' }
+  | { type: 'editing'; image: HTMLImageElement };
 
-  return image ? (
-    <EditorScreen
-      image={image}
-      onReset={() => {
-        setImage(null);
-      }}
-    />
-  ) : (
-    <UploadScreen onUpload={setImage} />
-  );
+export function ImageEditor() {
+  const [state, setState] = useState<State>({ type: 'upload' });
+
+  function handleUpload(file: File) {
+    setState({ type: 'loading' });
+
+    const objURL = URL.createObjectURL(file);
+    const image = new Image();
+    image.onload = () => {
+      setState({ type: 'editing', image });
+      URL.revokeObjectURL(objURL);
+    };
+    image.onerror = (e) => {
+      setState({ type: 'upload' });
+      URL.revokeObjectURL(objURL);
+      // TODO: UIに表示するよう変更
+      console.error(e);
+    };
+    image.src = objURL;
+  }
+
+  switch (state.type) {
+    case 'upload':
+      return <UploadScreen onUpload={handleUpload} />;
+
+    case 'loading':
+      return (
+        <section className="min-h-svh p-5">
+          <p className="bg-surface p-10 text-center">画像読み込み中...</p>
+        </section>
+      );
+
+    case 'editing':
+      return (
+        <EditorScreen
+          image={state.image}
+          onReset={() => {
+            setState({ type: 'upload' });
+          }}
+        />
+      );
+  }
 }
